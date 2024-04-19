fd0f4febb9da7f431ea92d448f64348715df01fc
KiksOfficial <151571662+KiksOfficial@users.noreply.github.com>
2024-04-19 17:49:05 +0300

    Update kontaktid.js

--- frontend/src/pages/kontaktid.js
+++ frontend/src/pages/kontaktid.js
@@ -1,9 +1,20 @@
 import '../styles/index.css'
+import Navbar from '../navbar'
 
 function Kontaktid() {
   return (
       <>
+      <Navbar />
       <h1>Kontaktid</h1>
+      <div className='kontaktid-card'>
+        <h1>Võtke meiega ühendust</h1>
+        <form action="https://formsubmit.co/marcusariston@gmail.com" method="POST">
+            <input type='text' placeholder='Nimi' name='Nimi'></input>
+            <input type='email' placeholder='Email' name='Email'></input>
+            <textarea placeholder='Sõnum' className='kontaktid-textarea' name='Sõnum'></textarea>
+            <button type='submit' className='kontaktid-button'>Saada</button>
+        </form>
+      </div>
       </> 
   )
 };

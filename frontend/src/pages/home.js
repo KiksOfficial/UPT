1f2ece4c88f96a9a8a59aee1c17a23dae7cac18f
KiksOfficial <151571662+KiksOfficial@users.noreply.github.com>
2024-04-19 08:20:44 +0300

    Update home.js

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -4,6 +4,24 @@ function Home() {
   return (
       <>
       <Navbar />
+      <div className='home-content'>
+
+      </div>
+      <div className='footer'>
+        <div className='footer-vasak'>
+          <p>example@gmail.com</p>
+        </div>
+        <div className='footer-kesk'>
+
+        </div>
+        <div className='footer-parem'>
+          <h3>Lingid</h3>
+          <a href='/'>Teenused</a>
+          <br/>
+          <a href='/'>Kontakt</a>
+        </div>
+        
+      </div>
       </>
   )
 };

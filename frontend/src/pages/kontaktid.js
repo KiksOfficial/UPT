389e00fd5347f663b372ff69cf3029d4b57bbc8a
KiksOfficial <151571662+KiksOfficial@users.noreply.github.com>
2024-04-21 13:21:15 +0300

    Update kontaktid.js

--- frontend/src/pages/kontaktid.js
+++ frontend/src/pages/kontaktid.js
@@ -1,31 +1,3 @@
-import '../styles/index.css'
-import Navbar from '../navbar'
-
-function Kontaktid() {
-  return (
-      <>
-      <Navbar />
-      <h1>Kontaktid</h1>
-      <div className='kontaktid-card'>
-        <h1>Võtke meiega ühendust</h1>
-        <form action="https://formsubmit.co/marcusariston@gmail.com" method="POST">
-            <input type='text' placeholder='Nimi' name='Nimi'></input>
-            <input type='email' placeholder='Email' name='Email' required></input>
-            <textarea placeholder='Sõnum' className='kontaktid-textarea' name='Sõnum'></textarea>
-            <button type='submit' className='kontaktid-button'>Saada</button>
-        </form>
-      </div>
-      </> 
-  )
-};
-
-export default Kontaktid;
-
-
-/*
-
-MDEA OSALISELT SAIN SELLE TOOLE A KUI EI SOBI SS ARA YLEMIST DELETE
-
 import React, { useState } from 'react';
 import '../styles/index.css';
 import Navbar from '../navbar';
@@ -72,5 +44,3 @@ function Kontaktid() {
 }
 
 export default Kontaktid;
-
-*/

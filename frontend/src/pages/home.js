2e3395604ca8def3880d5ccfceb6c885ad3c8611
KiksOfficial <151571662+KiksOfficial@users.noreply.github.com>
2024-04-24 11:59:05 +0300

    Update home.js

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -11,13 +11,17 @@ function Home() {
         <div className='home1'>
           <h1>Silmapaistva välimusega veebilehed mõistliku hinnaga!</h1>
           <p>
-          Pmst mingisugune kirjeldus i dunno + paremale peaks mingi pildi panema idk void teha mis tahad</p>
+          Loome silmapaistvaid veebilehti, mis haaravad külastajate tähelepanu ja pakuvad suurepärast kasutajakogemust. Meie fookuses on disaini ja funktsionaalsuse sümbioos, mis aitab teie ettevõttel võrgus esile tõusta. Usaldage meid oma veebipresenssi loomisel ja laske meie meeskonnal muuta teie visioon reaalsuseks.</p>
+          <a href='/kontaktid'>Võtke meiega ühendust</a>
         </div>
-        <div class="tilt">
-        <div class="tilt__content">
+        <div className="tilt">
+        <div className="tilt__content">
             <img src="https://bs-uploads.toptal.io/blackfish-uploads/components/seo/5819174/og_image/optimized/0717_Brutalist_and_Minimalist_Web_Design_Lina_Social-cbd0996a3f2bef8b9b10b86e0c4fb9e5.png" alt=""/>
         </div>
         </div>
+    </div>
+    <div className='home-kesk'>
+      
     </div>
 
     <script src="../tilt.js"></script>
